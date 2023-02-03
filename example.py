7af71c17452c5b8af68c5c941f5b7648ba5aea4d
tmatuszek <tmatuszek@seisen.com>
2023-02-03 13:45:24 +0900

    added comment for newDeal

--- example.py
+++ example.py
@@ -48,6 +48,7 @@ def startGame(numPlayers = 2, numCards = 5):
     :param numCards: the number of cards each player receives during each round
     '''
    # ADD CODE HERE
+   # newDeal()
 
 
 # ADD CODE HERE (Should be only 3 lines)
