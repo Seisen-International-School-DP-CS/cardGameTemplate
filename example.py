ed699f65011d78b1a6bde0b175398cdbb493d097
tmatuszek <tmatuszek@seisen.com>
2023-02-03 13:52:49 +0900

    .

--- example.py
+++ example.py
@@ -26,6 +26,7 @@ def newDeal():
         "A♣️", "2♣️", "3♣️", "4♣️", "5♣️", "6♣️", "7♣️", "8♣️", "9♣️", "10♣️", "J♣️", "Q♣️", "K♣️",
     ]
     # ADD CODE HERE
+    return deck
 
 def dealHand(currentDeck, numCards = 5):
     '''
@@ -41,13 +42,14 @@ def dealHand(currentDeck, numCards = 5):
 
 def startGame(numPlayers = 2, numCards = 5):
     '''
-    This function runs a card game, dealing out cards to players each round and reshuffling the deck if
+    This function runs a card game, dealing out cards to players each round and
+    reshuffling (refill by using the discarded card) the deck if
     there aren't enough cards left for all players that round.
-    It asks whether the user wants to continue after each round, and ends when they are done.
+    It asks whether the user wants to continue after each round, and ends when they are done (the user chooses to end).
     :param numPlayers: the number of players in this card game
     :param numCards: the number of cards each player receives during each round
     '''
    # ADD CODE HERE
-   # newDeatl()
+   #
 
 # ADD CODE HERE (Should be only 3 lines)
